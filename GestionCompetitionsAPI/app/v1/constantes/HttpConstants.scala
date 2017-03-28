bb5d38842584cfdb21ea48db236a9e3d54aa8812
JBEAUCOUSIN <JBEAUCOUSIN@LFR20635.INTRA.CORP.GRP>
2017-03-28 07:33:12 +0200

    Adding constant for query fields

---
+++ GestionCompetitionsAPI/app/v1/constantes/HttpConstants.scala
@@ -0,0 +1,13 @@
+package v1.constantes
+
+
+object QueryFields {
+  final val sort:String = "sort"
+  final val fields:String = "fields"
+  final val offset:String = "offset"
+  final val limit:String = "limit"
+}
+
+object HttpConstants {
+  final val queryFields = QueryFields  
+}
