16853e6bf749ab03d9d3779d25106bf100514c92
JEJE-Portable <JEJE-Portable@JEJE-PORTABLE>
2017-04-02 17:30:09 +0200

    Fix postal code issue

--- GestionCompetitionsAPI/app/v1/constantes/ValidationConstants.scala
+++ GestionCompetitionsAPI/app/v1/constantes/ValidationConstants.scala
@@ -3,7 +3,7 @@ package v1.constantes
 final object ValidationConstants {
   final object Regex {
     final val PASSWORD = """[a-zA-Z0-9@*#]{8,15}""".r
-    final val POSTAL_CODE = """[\d]{6}""".r
+    final val POSTAL_CODE = """[\d]{5}""".r
   }
 
   final val regex = Regex
